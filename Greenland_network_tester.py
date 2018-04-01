84268ac78fe5d17fa01655f2759e3af7d696bc1f
Lizz <ehultee@umich.edu>
2018-04-01 13:00:17 -0400

    Testing new functionality
    Re-compiled utility files (GL_model_tools, flowline_class_hierarchy) to test new functionality on Kangerlussuaq.

--- Greenland_network_tester.py
+++ Greenland_network_tester.py
@@ -53,54 +53,64 @@ B_interp = interpolate.RectBivariateSpline(X, Y[::-1], B.T[::, ::-1])
 ### FINDING GLACIERS
 ##-------------------
 
-jakcoords_0, jakcoords_1, jakcoords_2 = Flowline_CSV('Documents/1. Research/2. Flowline networks/Model/Greenland_tests/Flowline_sets/jakobshavn-thead_lines.csv', 3)
-jak_0 = Flowline(coords=jakcoords_0, index=0, name='Jak mainline')
-jak_1 = Flowline(coords=jakcoords_1, index=1, name='Jak line 1')
-jak_2 = Flowline(coords=jakcoords_2, index=2, name='Jak line 2')
-Jakobshavn_main = PlasticNetwork(name='Jakobshavn Isbrae [main]', init_type='Flowline', branches=(jak_0), main_terminus=jakcoords_0[0])
-Jakobshavn_1 = PlasticNetwork(name='Jakobshavn Isbrae 1', init_type='Flowline', branches=(jak_1), main_terminus=jakcoords_1[0])
-Jakobshavn_2 = PlasticNetwork(name='Jakobshavn Isbrae 2', init_type='Flowline', branches=(jak_2), main_terminus=jakcoords_2[0])
+#jakcoords_0, jakcoords_1, jakcoords_2 = Flowline_CSV('Documents/1. Research/2. Flowline networks/Model/Greenland_tests/Flowline_sets/jakobshavn-thead_lines.csv', 3)
+#jak_0 = Flowline(coords=jakcoords_0, index=0, name='Jak mainline')
+#jak_1 = Flowline(coords=jakcoords_1, index=1, name='Jak line 1')
+#jak_2 = Flowline(coords=jakcoords_2, index=2, name='Jak line 2')
+#Jakobshavn_main = PlasticNetwork(name='Jakobshavn Isbrae [main]', init_type='Flowline', branches=(jak_0), main_terminus=jakcoords_0[0])
+#Jakobshavn_1 = PlasticNetwork(name='Jakobshavn Isbrae 1', init_type='Flowline', branches=(jak_1), main_terminus=jakcoords_1[0])
+#Jakobshavn_2 = PlasticNetwork(name='Jakobshavn Isbrae 2', init_type='Flowline', branches=(jak_2), main_terminus=jakcoords_2[0])
+#
+#kbcoords = Flowline_CSV('Documents/1. Research/2. Flowline networks/Model/Greenland_tests/Flowline_sets/koge_bugt-thead_lines.csv', 1)[0]
+#kb_line = Flowline(coords=kbcoords, index=0, name='Koge Bugt mainline')
+#KogeBugt = PlasticNetwork(name='Koge Bugt', init_type='Flowline', branches=kb_line, main_terminus=kbcoords[0])
+#
+#
+#### INTERSECTING LINES - ADDITIONAL PROCESSING NEEDED
+#helcoords_0, helcoords_1, helcoords_2 = Flowline_CSV('Documents/1. Research/2. Flowline networks/Model/Greenland_tests/Flowline_sets/helheim-branch_lines.csv', 3)
+#hel_0 = Branch(coords=helcoords_0, index=0, order=0)
+#hel_1 = Branch(coords=helcoords_1, index=1, order=1, flows_to=0)
+#hel_2 = Branch(coords=helcoords_2, index=2, order=1, flows_to=0)
+#hel_branches = (hel_0, hel_1, hel_2)
+#Helheim = PlasticNetwork(name='Helheim', init_type='Branch', branches=hel_branches, main_terminus=helcoords_0[0])
+#Helheim.make_full_lines()
+#
+#
+#kangercoords_0, kangercoords_1, kangercoords_2, kangercoords_3, kangercoords_4, kangercoords_5 = Flowline_CSV('Documents/1. Research/2. Flowline networks/Model/Greenland_tests/Flowline_sets/kangerlussuaq-branch_lines.csv', 6)
+#kanger_0 = Branch(coords=kangercoords_0, index=0, order=0)
+#kanger_1 = Branch(coords=kangercoords_1, index=1, order=1, flows_to=0)
+##kanger_2 = Branch(coords=kangercoords_2, index=2, order=2, flows_to=1) #branch 2 did not connect under previous methodology.  Keep low expectations here
+#kanger_3 = Branch(coords=kangercoords_3, index=3, order=1, flows_to=0)
+#kanger_4 = Branch(coords=kangercoords_4, index=4, order=1, flows_to=0)
+#kanger_branches = (kanger_0, kanger_1, kanger_3, kanger_4)
+#Kanger = PlasticNetwork(name='Kangerlussuaq', init_type='Branch', branches=kanger_branches, main_terminus=kangercoords_0[0])
+#Kanger.make_full_lines()
+#
+##separate network for kanger_5, which has separate terminus
+#kanger_secondary = Flowline(coords=kangercoords_5, index=0)
+#Kanger_s = PlasticNetwork(name='Kangerlussuaq [secondary]', init_type='Flowline', branches=kanger_secondary, main_terminus=kangercoords_5[0])
 
-kbcoords = Flowline_CSV('Documents/1. Research/2. Flowline networks/Model/Greenland_tests/Flowline_sets/koge_bugt-thead_lines.csv', 1)[0]
-kb_line = Flowline(coords=kbcoords, index=0, name='Koge Bugt mainline')
-KogeBugt = PlasticNetwork(name='Koge Bugt', init_type='Flowline', branches=kb_line, main_terminus=kbcoords[0])
-
-
-### INTERSECTING LINES - ADDITIONAL PROCESSING NEEDED
-helcoords_0, helcoords_1, helcoords_2 = Flowline_CSV('Documents/1. Research/2. Flowline networks/Model/Greenland_tests/Flowline_sets/helheim-branch_lines.csv', 3)
-hel_0 = Branch(coords=helcoords_0, index=0, order=0)
-hel_1 = Branch(coords=helcoords_1, index=1, order=1, flows_to=0)
-hel_2 = Branch(coords=helcoords_2, index=2, order=1, flows_to=0)
-hel_branches = (hel_0, hel_1, hel_2)
-Helheim = PlasticNetwork(name='Helheim', init_type='Branch', branches=hel_branches, main_terminus=helcoords_0[0])
-Helheim.make_full_lines()
-
-
-kangercoords_0, kangercoords_1, kangercoords_2, kangercoords_3, kangercoords_4, kangercoords_5 = Flowline_CSV('Documents/1. Research/2. Flowline networks/Model/Greenland_tests/Flowline_sets/kangerlussuaq-branch_lines.csv', 6)
+kangercoords_0, kangercoords_1, kangercoords_2, kangercoords_3, kangercoords_4 = Flowline_CSV('kangerlussuaq-network-w_width.csv', 5, has_width=True, flip_order=False)
 kanger_0 = Branch(coords=kangercoords_0, index=0, order=0)
-kanger_1 = Branch(coords=kangercoords_1, index=1, order=1, flows_to=0)
-#kanger_2 = Branch(coords=kangercoords_2, index=2, order=2, flows_to=1) #branch 2 did not connect under previous methodology.  Keep low expectations here
-kanger_3 = Branch(coords=kangercoords_3, index=3, order=1, flows_to=0)
-kanger_4 = Branch(coords=kangercoords_4, index=4, order=1, flows_to=0)
+kanger_1 = Branch(coords=kangercoords_1, index=1, order=1, flows_to=0, intersect=174)
+kanger_2 = Branch(coords=kangercoords_2, index=2, order=1, flows_to=0, intersect=191) #DIFFERENT FROM PREVIOUS BRANCH 2.  NEW FLOWLINE SET AS OF 31 MAR 2018
+kanger_3 = Branch(coords=kangercoords_3, index=3, order=1, flows_to=0, intersect=146)
+kanger_4 = Branch(coords=kangercoords_4, index=4, order=1, flows_to=0, intersect=61)
 kanger_branches = (kanger_0, kanger_1, kanger_3, kanger_4)
 Kanger = PlasticNetwork(name='Kangerlussuaq', init_type='Branch', branches=kanger_branches, main_terminus=kangercoords_0[0])
-Kanger.make_full_lines()
-
-#separate network for kanger_5, which has separate terminus
-kanger_secondary = Flowline(coords=kangercoords_5, index=0)
-Kanger_s = PlasticNetwork(name='Kangerlussuaq [secondary]', init_type='Flowline', branches=kanger_secondary, main_terminus=kangercoords_5[0])
+#Kanger.make_full_lines()
 
 
 ##-------------------
 ### PROCESSING LINE FUNCTIONS
 ##-------------------
-for gl in (Jakobshavn_main, Jakobshavn_1, Jakobshavn_2, KogeBugt, Helheim, Kanger, Kanger_s):
-    gl.process_full_lines(B_interp, S_interp, H_interp)
-    if gl in (Kanger, Kanger_s): # Add more sophisticated code to catch warnings?
-        gl.remove_floating()
-        gl.make_full_lines()
-        gl.process_full_lines(B_interp, S_interp, H_interp)
-#    gl.optimize_network_yield(check_all=True)
+#for gl in (Jakobshavn_main, Jakobshavn_1, Jakobshavn_2, KogeBugt, Helheim, Kanger, Kanger_s):
+#    gl.process_full_lines(B_interp, S_interp, H_interp)
+#    if gl in (Kanger, Kanger_s): # Add more sophisticated code to catch warnings?
+#        gl.remove_floating()
+#        gl.make_full_lines()
+#        gl.process_full_lines(B_interp, S_interp, H_interp)
+##    gl.optimize_network_yield(check_all=True)
 
 
 ##debugging Kangerlussuaq
