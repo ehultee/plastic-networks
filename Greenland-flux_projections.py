Fixes to flux calculation - include all branches
Small edits to flowline.icediff to correctly calculate flux once terminus has retreated past intersection points.

Saving Greenland-flux_projections before modifying its file permissions in Terminal window

Greenland-flux_projections.py
@@ -74,20 +74,20 @@ B_interp = interpolate.RectBivariateSpline(X, Y[::-1], smoothB.T[::, ::-1])
 #vf_y = interpolate.interp2d(xv, yv[::-1], vy_upper[::-1,::])
 #vf = interpolate.interp2d(xv, yv[::-1], v_upper[::-1, ::])
 
-print 'Reading in 5-year surface elevation change'
-gl_sec_path ='Documents/GitHub/plastic-networks/Data/CS2-SEC_5yr.nc'
-#gl_sec_path ='Documents/GitHub/plastic-networks/Data/CS2-SEC_2yr.nc'
-fh3 = Dataset(gl_sec_path, mode='r')
-x_sec = fh3.variables['x'][:].copy() #x-coord (polar stereo)
-y_sec = fh3.variables['y'][:].copy() #y-coord (polar stereo)
-t_sec = fh3.variables['t'][:].copy() #average time of slice (days since 1 JAN 2000)
-sec_raw = fh3.variables['SEC'][:].copy()
-fh3.close()
-
-sec_i_masked = np.ma.masked_greater(sec_raw[:,:,0], 9000)
-sec_i_excludemasked = np.ma.filled(sec_i_masked, fill_value=np.mean(sec_i_masked))
-#sec_i_regrid = interpolate.griddata((x_sec.ravel(), y_sec.ravel()), sec_i_masked.ravel(), (Xmat, Ymat), method='nearest')
-SEC_i = interpolate.RectBivariateSpline(x_sec, y_sec, sec_i_excludemasked.T)
+#print 'Reading in 5-year surface elevation change'
+#gl_sec_path ='Documents/GitHub/plastic-networks/Data/CS2-SEC_5yr.nc'
+##gl_sec_path ='Documents/GitHub/plastic-networks/Data/CS2-SEC_2yr.nc'
+#fh3 = Dataset(gl_sec_path, mode='r')
+#x_sec = fh3.variables['x'][:].copy() #x-coord (polar stereo)
+#y_sec = fh3.variables['y'][:].copy() #y-coord (polar stereo)
+#t_sec = fh3.variables['t'][:].copy() #average time of slice (days since 1 JAN 2000)
+#sec_raw = fh3.variables['SEC'][:].copy()
+#fh3.close()
+#
+#sec_i_masked = np.ma.masked_greater(sec_raw[:,:,0], 9000)
+#sec_i_excludemasked = np.ma.filled(sec_i_masked, fill_value=np.mean(sec_i_masked))
+##sec_i_regrid = interpolate.griddata((x_sec.ravel(), y_sec.ravel()), sec_i_masked.ravel(), (Xmat, Ymat), method='nearest')
+#SEC_i = interpolate.RectBivariateSpline(x_sec, y_sec, sec_i_excludemasked.T)
 
 
 ##-------------------
@@ -128,7 +128,7 @@ Helheim.load_network(filename='Helheim.pickle')
 kangercoords_0, kangercoords_1, kangercoords_2, kangercoords_3, kangercoords_4 = Flowline_CSV('Documents/GitHub/plastic-networks/Data/kangerlussuaq-network-w_width.csv', 5, has_width=True, flip_order=False)
 kanger_0 = Branch(coords=kangercoords_0, index=0, order=0)
 kanger_1 = Branch(coords=kangercoords_1, index=1, order=1, flows_to=0, intersect=174)
-kanger_2 = Branch(coords=kangercoords_2, index=2, order=1, flows_to=0, intersect=191) #DIFFERENT FROM PREVIOUS BRANCH 2.  NEW FLOWLINE SET AS OF 31 MAR 2018
+#kanger_2 = Branch(coords=kangercoords_2, index=2, order=1, flows_to=0, intersect=191) #DIFFERENT FROM PREVIOUS BRANCH 2.  NEW FLOWLINE SET AS OF 31 MAR 2018
 kanger_3 = Branch(coords=kangercoords_3, index=3, order=1, flows_to=0, intersect=146)
 kanger_4 = Branch(coords=kangercoords_4, index=4, order=1, flows_to=0, intersect=61)
 kanger_branches = (kanger_0, kanger_1, kanger_3, kanger_4)
@@ -150,6 +150,7 @@ for gl in glacier_networks:
         gl.remove_floating()
         if gl in (Kanger,):
             gl.make_full_lines()
+            #gl.flowlines = gl.flowlines[4::] #ersatz fix for now - need to ensure that make_full_lines overwrites flowlines rather than adds new ones
         else:
             pass
         gl.process_full_lines(B_interp, S_interp, H_interp)
@@ -161,42 +162,11 @@ for gl in glacier_networks:
 
 
 ###-------------------
-#### FORWARD PROJECTION--FORCING FROM UPSTREAM
-###-------------------
-
-#testyears = arange(20)
-#
-#jak_rate = 10/H0 #m/a, mid/low end from Csatho 2008
-#kb_rate = 2/H0
-#hel_rate = 5/H0
-#kanger_rate = 5/H0
-#thinrates = [jak_rate, kb_rate, hel_rate, kanger_rate]
-#
-#Jakobshavn_main.network_time_evolve(testyears=testyears, thinrate=jak_rate)
-#
-#KogeBugt.network_time_evolve(testyears=testyears, thinrate=kb_rate)
-#
-#Helheim.network_time_evolve(testyears=testyears, thinrate=hel_rate)
-#
-#Kanger.network_time_evolve(testyears=testyears, thinrate=kanger_rate)
-
-###-------------------
 #### FORWARD PROJECTION--FORCING FROM TERMINUS
 ###-------------------
 
-## Finding balance rate as basis for future accumulation
-#jak_balance = Jakobshavn_main.balance_adot(vf)
-#kb_balance = KogeBugt.balance_adot(vf)
-#hel_balance = Helheim.balance_adot(vf)
-#kanger_balance = Kanger.balance_adot(vf)
-#
-#forcingrates = [-2*r for r in (jak_balance, kb_balance, hel_balance, kanger_balance)]
-#coarsety = arange(10, step=0.05)
-#finety = arange(10, 15, step=0.05)
-#testyears = np.concatenate((coarsety, finety))
-
-testyears = arange(25, step=0.10)
-db=True
+testyears = arange(100, step=0.25)
+db=False
 
 #testglac = (Jakobshavn_main,)
 testglac = glacier_networks #to test all
@@ -205,12 +175,12 @@ testglac = glacier_networks #to test all
 #Finding SEC rates and making persistence projection
 for gl in testglac:
     print gl.name
-    gl.sec_mainline = np.asarray([SEC_i(gl.flowlines[0].coords[i,0], gl.flowlines[0].coords[i,1]) for i in range(len(gl.flowlines[0].coords))])
-    away_from_edge = np.argmin(gl.sec_mainline)
-    gl.sec_alphadot = np.mean(gl.sec_mainline[away_from_edge::])
+    #gl.sec_mainline = np.asarray([SEC_i(gl.flowlines[0].coords[i,0], gl.flowlines[0].coords[i,1]) for i in range(len(gl.flowlines[0].coords))])
+    #away_from_edge = np.argmin(gl.sec_mainline)
+    #gl.sec_alphadot = np.mean(gl.sec_mainline[away_from_edge::])
     #variable_forcing = linspace(start=gl.sec_alphadot, stop=2*gl.sec_alphadot, num=len(testyears))
-    gl.terminus_sec = float(min(gl.sec_mainline.flatten()))#using min because values close to edge get disrupted by mask interpolation
-    gl.terminus_time_evolve(testyears=testyears, alpha_dot=0, dL=1/L0, has_smb=True, terminus_balance=0, submarine_melt = 0, debug_mode=db) 
+    #gl.terminus_sec = float(min(gl.sec_mainline.flatten()))#using min because values close to edge get disrupted by mask interpolation
+    gl.terminus_time_evolve(testyears=testyears, alpha_dot=0, dL=1/L0, has_smb=False, terminus_balance=0, submarine_melt = 0, debug_mode=db, rate_factor=3.7E-26) 
     
     print 'Saving output for {}'.format(gl.name)
     fn = str(gl.name)
@@ -218,120 +188,144 @@ for gl in testglac:
     fn2 = fn1.replace("[", "-")
     fn3 = fn2.replace("/", "_")
     fn4 = fn3.replace("]", "")
-    fn5 = fn4+'8Jun18-0forcing.pickle'
+    fn5 = fn4+'-11Jun18-0forcing-coldice-100a_dt025a.pickle'
     gl.save_network(filename=fn5)
 
-#Kanger_multibranch_flux = [Kanger.model_output[j]['Terminus_flux'] for j in range(len(Kanger.flowlines))]
+#Kanger_multibranch_flux = [np.absolute(Kanger.model_output[j]['Terminus_flux']) for j in range(len(Kanger.flowlines))]
 #Kanger_total_flux = sum(Kanger_multibranch_flux, axis = 0) #note that Kanger_multibranch_flux is multidimensional, needs care in summing
 #Helheim_multibranch_flux = [Helheim.model_output[j]['Terminus_flux'] for j in range(len(Helheim.flowlines))]
 #Helheim_total_flux = sum(Helheim_multibranch_flux, axis=0)
 #Jakobshavn_multibranch_flux = [Jakobshavn_main.model_output[0]['Terminus_flux'], Jakobshavn_sec.model_output[0]['Terminus_flux'], Jakobshavn_tert.model_output[0]['Terminus_flux']]
 #Jakobshavn_total_flux = sum(Jakobshavn_multibranch_flux, axis=0)
-#fluxes = [Jakobshavn_main.model_output[0]['Terminus_flux'], Jakobshavn_sec.model_output[0]['Terminus_flux'], Jakobshavn_tert.model_output[0]['Terminus_flux'], KogeBugt.model_output[0]['Terminus_flux'], Helheim.model_output[0]['Terminus_flux'], Kanger.model_output[0]['Terminus_flux']]
+#main_fluxes = [Jakobshavn_main.model_output[0]['Terminus_flux'], Jakobshavn_sec.model_output[0]['Terminus_flux'], Jakobshavn_tert.model_output[0]['Terminus_flux'], KogeBugt.model_output[0]['Terminus_flux'], Helheim.model_output[0]['Terminus_flux'], Kanger.model_output[0]['Terminus_flux']] #main calving termini only
 #total_fluxes = [Jakobshavn_total_flux, KogeBugt.model_output[0]['Terminus_flux'], Helheim_total_flux, Kanger_total_flux]
+#total_fluxes_split = [Jakobshavn_main.model_output[0]['Terminus_flux'], Jakobshavn_sec.model_output[0]['Terminus_flux'], Jakobshavn_tert.model_output[0]['Terminus_flux'], KogeBugt.model_output[0]['Terminus_flux'], Helheim_total_flux, Kanger_total_flux] #totals from all termini, with Jak split by branch (network)
 #
 #fluxes_cleaned = []
 #sle = [] #will be array of annual sea level contributions
-#for flux in total_fluxes:
+#for flux in total_fluxes_split:
 #    flux_c = np.nan_to_num(flux)
-#    fluxes_cleaned.append(flux_c)
-#    sleq = (1E-12)*np.array(flux_c)/(361.8) #Gt ice / mm sea level equiv conversion
+#    flux_a = np.absolute(flux_c)
+#    fluxes_cleaned.append(flux_a)
+#    sleq = (1E-12)*np.array(flux_a)/(361.8) #Gt ice / mm sea level equiv conversion
 #    sle.append(sleq)
 #cumul_sle_pernetwork = []
 ##total_sle = []
 #for sl in sle:
 #    c = np.cumsum(sl)
 #    cumul_sle_pernetwork.append(c)
-#total_sle = np.cumsum(cumul_sle_pernetwork, axis=1)
-
-###-------------------
-#### SUMMARY PLOTTING
-###-------------------
-
+#total_sle = np.cumsum(cumul_sle_pernetwork, axis=0)
+#
+#### Splitting by branch for Jakobshavn
+##fluxes_cleaned = []
+##sle = []
+##for flux in total_fluxes_split:
+##    flux_c = np.nan_to_num(flux)
+##    flux_a = np.absolute(flux_c)
+##    fluxes_cleaned.append(flux_a)
+##    sleq = (1E-12)*np.array(flux_a)/(361.8)
+##    sle.append(sleq)
+##cumul_sle_split = []
+##for sl in sle:
+##    c = np.cumsum(sl)
+##    cumul_sle_split.append(c)
+##total_sle_split = np.cumsum(cumul_sle_split, axis=0)
+##
+######-------------------
+####### SUMMARY PLOTTING
+######-------------------
+##
 #projections = [Jakobshavn_main.model_output, Jakobshavn_sec.model_output, Jakobshavn_tert.model_output, KogeBugt.model_output, Helheim.model_output, Kanger.model_output]
 #names = ['Sermeq Kujalleq [main]', 'Sermeq Kujalleq [central]', 'Sermeq Kujalleq [north]', 'Koge Bugt', 'Helheim', 'Kangerlussuaq']
 #combined_networks = ['Sermeq Kujalleq', 'Koge Bugt', 'Helheim', 'Kangerlussuaq']
-#rates = ['{0:.2f} m/a'.format(gl.sec_alphadot) for gl in glacier_networks]
+##rates = ['{0:.2f} m/a'.format(gl.sec_alphadot) for gl in glacier_networks]
 ###styles = [':', '-.', '--', '-']
 #markers = ['o', '.', ',', '^', 'd', '*']
 #styles = ['-', ':', '-.', '-', '-', '-']
 #cmap = matplotlib.cm.get_cmap('winter')
 #colors = cmap([0.1, 0.2, 0.3, 0.5, 0.7, 0.9])
 #alt_colors = matplotlib.cm.get_cmap('Greys')([0.3, 0.5, 0.7, 0.9])
-
-
-##terminus
-#plt.figure()
+##
+##
+###terminus
+#plt.figure('Terminus retreat to 2100', figsize=(12,8))
 #for j, pr in enumerate(projections):
 #    print j
-#    plt.plot(testyears, -0.001*np.array(pr[0]['Termini'][1::]), linewidth=4, color=colors[j], linestyle=styles[j], label='{}, {}'.format(names[j], rates[j]))
+#    plt.plot(testyears, -0.001*np.array(pr[0]['Termini'][1::]), linewidth=4, color=colors[j], linestyle=styles[j], label='{}'.format(names[j]))
 #    plt.plot(testyears[::20], -0.001*np.array(pr[0]['Termini'][1::])[::20], linewidth=0, marker=markers[j], ms=10, color=colors[j])
 #plt.legend(loc='lower left')
 #plt.axes().set_xlabel('Year of simulation', size=20)
 #plt.axes().set_ylabel('Terminus change [km]', size=20)
 #plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
-##plt.axes().set_ylim(-12, 1)
-##plt.axes().set_yticks([-10, -5, 0])
-#plt.title('25 a persistence 2011-2017 $\Delta$SE + 100 m/a submarine melt - dt=0.10 a - Gaussian sigma=2 - A=1.7E-24 - reduced & dimensional dL', fontsize=18)
+#plt.axes().set_xlim(0, 82)
+##plt.axes().set_xticks([7, 32, 57, 82])
+##plt.axes().set_xticklabels(['2025', '2050', '2075', '2100']) #label projection out to calendar year 2100
+#plt.axes().set_ylim(-100, 1)
+#plt.axes().set_yticks([-100, -75, -50, -25, 0])
+#plt.title('Dynamic thinning to 2100, warm ice', fontsize=26)
 #plt.show()
-
-###JAKOBSHAVN ONLY - checking monotonicity
-#plt.figure('Koge Bugt terminus change')
-##for k, mo in enumerate(Helheim.model_output): #for each branch j
-#mo = KogeBugt.model_output[0]
-#plt.plot(0.1*arange(len(mo['Termini'])), -0.001*np.array(mo['Termini']), linewidth=4, color='k', label='Koge Bugt mainline')
-#plt.plot(0.1*arange(len(mo['Termini']))[::5], -0.001*np.array(mo['Termini'])[::5], linewidth=0, color='k', marker='.', ms=10)
-#plt.legend(loc='lower left')
-#plt.axes().set_xlabel('Year of simulation', size=30)
-#plt.axes().set_ylabel('Terminus change [km]', size=30)
+##
+#####JAKOBSHAVN ONLY - checking monotonicity
+####plt.figure('Koge Bugt terminus change')
+#####for k, mo in enumerate(Helheim.model_output): #for each branch j
+####mo = KogeBugt.model_output[0]
+####plt.plot(0.1*arange(len(mo['Termini'])), -0.001*np.array(mo['Termini']), linewidth=4, color='k', label='Koge Bugt mainline')
+####plt.plot(0.1*arange(len(mo['Termini']))[::5], -0.001*np.array(mo['Termini'])[::5], linewidth=0, color='k', marker='.', ms=10)
+####plt.legend(loc='lower left')
+####plt.axes().set_xlabel('Year of simulation', size=30)
+####plt.axes().set_ylabel('Terminus change [km]', size=30)
+####plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
+#####plt.axes().set_ylim(-16, 1)
+#####plt.axes().set_yticks([-15, -10, -5, 0])
+####plt.show()
+###
+#####SINGLE NETWORK - splitting termini
+#plt.figure('Helheim terminus change')
+#for k, mo in enumerate(Helheim.model_output): #for each branch j
+#    #colork = matplotlib.cm.get_cmap('viridis')(k/len(Helheim.model_output))
+#    #markerk = (k+2, mod(k+1, 3), 0)
+#    lsk = (':', '-.', '--')
+#    plt.plot(testyears[::], -0.001*np.array(mo['Termini'])[:-1:], linewidth=4, color='k', ls=lsk[k], label='Helheim line {}'.format(k))
+#    #plt.plot(testyears[::10], -0.001*np.array(mo['Termini'])[:-1:10], linewidth=0, color='k', marker=markerk, ms=10, label='Helheim line {}'.format(k))
+#plt.legend(loc='upper right')
+#plt.axes().set_xlabel('Year of simulation', size=20)
+#plt.axes().set_ylabel('Terminus change [km]', size=20)
 #plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
 ##plt.axes().set_ylim(-16, 1)
 ##plt.axes().set_yticks([-15, -10, -5, 0])
 #plt.show()
-
-###SINGLE NETWORK - splitting termini
-#plt.figure('Kangerlussuaq terminus change')
-#for k, mo in enumerate(Kanger.model_output): #for each branch j
-#    colork = matplotlib.cm.get_cmap('viridis')(k/len(Kanger.model_output))
-#    markerk = (k+2, mod(k+1, 3), 0)
-#    plt.plot(0.1*arange(len(mo['Termini'])), -0.001*np.array(mo['Termini']), linewidth=4, color='k')
-#    plt.plot(0.1*arange(len(mo['Termini']))[::10], -0.001*np.array(mo['Termini'])[::10], linewidth=0, color='k', marker=markerk, ms=10, label='Kangerlussuaq line {}'.format(k))
-##plt.legend(loc='lower left')
-#plt.axes().set_xlabel('Year of simulation', size=30)
-#plt.axes().set_ylabel('Terminus change [km]', size=30)
-#plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
-##plt.axes().set_ylim(-16, 1)
-##plt.axes().set_yticks([-15, -10, -5, 0])
-#plt.show()
-
-###Flux
-#plt.figure()
+###
+#######Flux
+#plt.figure(figsize=(12,8))
 #for j in range(len(names)):
-#    plt.plot(testyears, 1E-12*np.array(fluxes[j]), linewidth=4, linestyle=styles[j], color=colors[j], label=names[j])
-#    plt.plot(testyears[::50], 1E-12*np.array(fluxes[j][::50]), linewidth=0, marker=markers[j], ms=10, color=colors[j])
-#    plt.fill_between(testyears, y1=1E-12*np.array(fluxes[j]), y2=0, color=colors[j], alpha=0.5)    
-#plt.legend(loc='lower left')
+#    plt.plot(testyears, 1E-12*np.array(fluxes_cleaned[j]), linewidth=4, linestyle=styles[j], color=colors[j], label=names[j])
+#    plt.plot(testyears[::50], 1E-12*np.array(fluxes_cleaned[j][::50]), linewidth=0, marker=markers[j], ms=10, color=colors[j])
+#    plt.fill_between(testyears, y1=1E-12*np.array(fluxes_cleaned[j]), y2=0, color=colors[j], alpha=0.5)    
+##plt.legend(loc='upper right')
 #plt.axes().set_xlabel('Year of simulation', size=20)
 #plt.axes().set_ylabel('Terminus ice flux [Gt/a]', size=20)
 #plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
 ##plt.axes().set_xlim(0,15.5)
 ##plt.axes().set_xticks([0,5,10, 15])
-##plt.axes().set_ylim(-16, 1)
-##plt.axes().set_yticks([-15, -10, -5, 0])
+##plt.axes().set_ylim(0, 18)
+##plt.axes().set_yticks([0, 5, 10, 15])
 #plt.show()
-#
-####Sea level equivalent
-#plt.figure()
-#for j in range(len(combined_networks)):
-#    plt.plot(1+testyears[::], total_sle[j], linewidth=4, color=alt_colors[j], label=combined_networks[j])
-#    plt.plot(1+testyears[::5], total_sle[j][::5], linewidth=0, marker=markers[j], ms=10, color=alt_colors[j])
-#    plt.fill_between(1+testyears[::], y1=total_sle[j], y2=0, color=alt_colors[j], alpha=0.7)    
+#####
+#########Sea level equivalent
+#plt.figure(figsize=(12,8))
+#for j in range(len(names)):
+#    plt.plot(1+testyears[::], total_sle[j], linewidth=4, color=colors[::][j], label=names[j])
+#    plt.plot(1+testyears[::5], total_sle[j][::5], linewidth=0, marker=markers[j], ms=10, color=colors[::][j])
+#    if j==0:
+#        plt.fill_between(1+testyears[::], y1=total_sle[j], y2=0, color=colors[::][j], alpha=0.7)  
+#    else:
+#        plt.fill_between(1+testyears[::], y1=total_sle[j], y2=total_sle[j-1], color=colors[::][j], alpha=0.7)      
 #plt.legend(loc='upper left')
-#plt.axes().set_xlabel('Year of simulation', size=30)
-#plt.axes().set_ylabel('Cumulative sea level contribution [mm]', size=30)
+#plt.axes().set_xlabel('Year of simulation', size=20)
+#plt.axes().set_ylabel('Cumulative sea level contribution [mm]', size=20)
 #plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
-#plt.axes().set_xlim(0, 25)
-#plt.axes().set_xticks([0, 5, 10, 15, 20, 25])
+#plt.axes().set_xlim(0, 100)
+#plt.axes().set_xticks([0, 25, 50, 75, 100])
 ##plt.axes().set_ylim(-16, 1)
 ##plt.axes().set_yticks([0, 1, 2, 3, 4])
 #plt.show()

flowline_class_hierarchy.py
@@ -267,10 +267,11 @@ class Flowline(Ice):
         except ValueError: #this happens when terminus retreats past upstream forcing point
             interpolated_func2 = lambda x: np.nan #return NaN once no longer calculating meaningful changes
     
-        if upstream_lim is None:
-            upstream_lim = 1.5
         arcmax = self.length
-        upstream_limit = min(upstream_lim, arcmax)
+                
+        if upstream_lim is None:
+            upstream_lim = arcmax
+        upstream_limit = min(upstream_lim, arcmax) #catches if a uniform upstream limit has been applied that exceeds length of a particular flowline
         
         if self.index != 0:
             idx = self.intersections[1] #for now will only catch one intersection...can refine later for more complex networks
@@ -378,7 +379,7 @@ class Flowline(Ice):
         """Function to compute terminus rate of advance/retreat given a mass balance forcing, a_dot.
         Input:
             profile: a plastic profile output from Flowline.plasticprofile of the current time step
-            alpha_dot: net rate of ice accumulation/loss.  Should be expressed in m/a /H0. Spatially averaged over whole catchment for now
+            alpha_dot: net rate of ice accumulation/loss.  Should be expressed in m/a. Spatially averaged over whole catchment for now
             rate_factor: flow rate factor A, assumed 3.5x10^(-25) Pa^-3 s^-1 for T=-10C, or 1.7E-24 for T=-2C, based on Cuffey & Paterson
         Optional inputs:
             dL: passed to Flowline.find_dHdL as length of step over which to test dHdL profiles.  Default 5 m.
@@ -495,9 +496,11 @@ class PlasticNetwork(Ice):
     
     def make_full_lines(self):
         """Runs spatial KDTree search to connect flowlines at nearest points. Creates Flowline objects that reach from each branch head to the terminus.
-        
+        Note: this will overwrite any flowlines already saved to the network.
         """
-        #KDTree search just connects at closest points...may still be wonky
+        self.flowlines = []
+        
+        #KDTree search connects at closest points...may still be wonky
         #project somehow with Shapely?
         mainline = self.branches[0].full_coords
         maintree = spatial.KDTree(mainline[:,0:2])
@@ -750,7 +753,7 @@ class PlasticNetwork(Ice):
         self.balance_forcing = float(balance_a) #save to this network instance
         return balance_a     
     
-    def terminus_time_evolve(self, testyears=arange(100), ref_branch_index=0, alpha_dot=None, alpha_dot_variable=None, upstream_limits=None, use_mainline_tau=True, debug_mode=False, dt_rounding=5, dL=None, has_smb=False, terminus_balance=None, submarine_melt=0):
+    def terminus_time_evolve(self, testyears=arange(100), ref_branch_index=0, alpha_dot=None, alpha_dot_variable=None, upstream_limits=None, use_mainline_tau=True, debug_mode=False, dt_rounding=5, dL=None, has_smb=False, terminus_balance=None, submarine_melt=0, rate_factor=1.7E-24):
         """Time evolution on a network of Flowlines, forced from terminus.  Lines should be already optimised and include reference profiles from network_ref_profiles
         Arguments:
             testyears: a range of years to test, indexed by years from nominal date of ref profile (i.e. not calendar years)
@@ -812,9 +815,9 @@ class PlasticNetwork(Ice):
             key = round(yr-dt, dt_rounding) #allows dictionary referencing when dt is < 1 a
             
             if k<1:
-                dLdt_annum = ref_line.dLdt_dimensional(profile=refdict[0], alpha_dot=alpha_dot_k, debug_mode=debug_mode, dL=dL, has_smb=has_smb, terminus_balance=terminus_balance, submarine_melt=submarine_melt)
+                dLdt_annum = ref_line.dLdt_dimensional(profile=refdict[0], alpha_dot=alpha_dot_k, debug_mode=debug_mode, dL=dL, has_smb=has_smb, terminus_balance=terminus_balance, submarine_melt=submarine_melt, rate_factor=rate_factor)
             else:
-                dLdt_annum = ref_line.dLdt_dimensional(profile=refdict[key], alpha_dot=alpha_dot_k, debug_mode=debug_mode, dL=dL, has_smb=has_smb, terminus_balance=terminus_balance, submarine_melt=submarine_melt)
+                dLdt_annum = ref_line.dLdt_dimensional(profile=refdict[key], alpha_dot=alpha_dot_k, debug_mode=debug_mode, dL=dL, has_smb=has_smb, terminus_balance=terminus_balance, submarine_melt=submarine_melt, rate_factor=rate_factor)
             if np.isnan(dLdt_annum): #happens if retreat hits edge of domain unexpectedly
                 dLdt_annum = refdict['Termrates'][-1] / dt #replace with last non-nan value
             else:
@@ -877,7 +880,7 @@ class PlasticNetwork(Ice):
                         branch_terminus = new_termpos
                         branch_termheight = new_termheight
                     else: ##if branches have split, find new terminus quantities
-                        dLdt_branch = fl.dLdt_dimensional(profile=out_dict[key], alpha_dot=alpha_dot_k, debug_mode=debug_mode, dL=dL, has_smb=has_smb, terminus_balance=terminus_balance, submarine_melt=submarine_melt)
+                        dLdt_branch = fl.dLdt_dimensional(profile=out_dict[key], alpha_dot=alpha_dot_k, debug_mode=debug_mode, dL=dL, has_smb=has_smb, terminus_balance=terminus_balance, submarine_melt=submarine_melt, rate_factor=rate_factor)
                         if np.isnan(dLdt_branch):
                             dLdt_branch = out_dict['Termrates'][-1] /dt
                         else:

terminus_forcing_tests.py
@@ -612,7 +612,7 @@ def terminus_time_evolve(network, testyears=arange(100), ref_branch_index=0, a_d
 #total_dLdt_ma = (ref_line.L0 /ref_line.T_0) * np.array(total_dLdt)
 
 
-def analyse_dLdt_dimensional(flowline, profile, alpha_dot, rate_factor=1.7E-24, dL=None, debug_mode=False, has_smb=False, terminus_balance=None, submarine_melt=0):
+def analyse_dLdt_dimensional(flowline, profile, alpha_dot, rate_factor=3.5E-25, dL=None, debug_mode=False, has_smb=False, terminus_balance=None, submarine_melt=0):
     """Function to compute terminus rate of advance/retreat given a mass balance forcing, a_dot.
     Input:
         profile: a plastic profile output from Flowline.plasticprofile of the current time step
@@ -710,7 +710,7 @@ def analyse_dLdt_dimensional(flowline, profile, alpha_dot, rate_factor=1.7E-24, 
 ref_line = Jakobshavn_main.flowlines[0]
 ref_surface = ref_line.ref_profile
 ref_amax = 0.5*ref_line.length
-testpositions = linspace(0, ref_amax, num=100)
+testpositions = linspace(0, ref_amax, num=50)
 terminus_SE = [ref_surface(0)]
 
 total_dLdt = []