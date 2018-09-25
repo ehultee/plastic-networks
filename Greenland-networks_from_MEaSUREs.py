acf7a2e7cef53bcfc0d1ea5ad89b876061e0246e
Lizz <ehultee@umich.edu>
2018-09-25 18:56:14 -0400

    Adjusted units of MEaSUREs velocity for tracing
    Converted MEaSUREs velocities into m/day (instead of m/annum) to work as expected with network selection presets.
    
    Also down-sampled terminus points to be traced.  MEaSUREs terminus collections quite dense and it requires a lot of computational power to trace all of them.

--- Greenland-networks_from_MEaSUREs.py
+++ Greenland-networks_from_MEaSUREs.py
@@ -7,6 +7,7 @@ from scipy.signal import savgol_filter #use Savitzky-Golay filter to smooth catc
 from osgeo import gdal
 import sys #allowing GDAL to throw Python exceptions
 import pandas as pd #want to treat velocity maps as Pandas dataframes
+import shapefile
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from matplotlib import cm
@@ -59,45 +60,63 @@ vy_comp = np.ma.masked_invalid(vy_comp_raw)
 v_excludemasked = np.ma.filled(v_comp, fill_value=0)
 vx_excludemasked = np.ma.filled(vx_comp, fill_value=0)
 vy_excludemasked = np.ma.filled(vy_comp, fill_value=0)
+days_per_annum = 365.242 #need to convert units of MEaSUREs velocity to align with what we used from Sentinel before
+v_a2d = np.array(v_excludemasked) / days_per_annum
+vx_a2d = np.array(vx_excludemasked) / days_per_annum
+vy_a2d = np.array(vy_excludemasked) / days_per_annum
 
 
 ##Make 2D-interpolated function of velocity field for tracing
 print 'Interpolating MEaSUREs velocity composites for tracing'
-#xm = x_comp[~v_comp.mask]
-#ym = y_comp[~v_comp.mask]
 x_flat = x_comp[0,:]
 y_flat = y_comp[:,0]
-func_vxcomp = interpolate.interp2d(x_flat, y_flat[::-1], vx_excludemasked) #check whether these need to be flipped along y-axis
-func_vycomp = interpolate.interp2d(x_flat, y_flat[::-1], vy_excludemasked)
-func_vcomp = interpolate.interp2d(x_flat, y_flat[::-1], v_excludemasked)
+func_vxcomp = interpolate.interp2d(x_flat, y_flat[::-1], vx_a2d) #these need to be flipped along y-axis
+func_vycomp = interpolate.interp2d(x_flat, y_flat[::-1], vy_a2d)
+func_vcomp = interpolate.interp2d(x_flat, y_flat[::-1], v_a2d)
 
-xtest = np.linspace(min(x_flat), max(x_flat), 1000)
-ytest = np.linspace(min(y_flat), max(y_flat), 500)
-plt.figure()
-plt.contour(xtest, ytest, func_vcomp(xtest, ytest))
-plt.show()
+#xtest = np.linspace(min(x_flat), max(x_flat), 1000)
+#ytest = np.linspace(min(y_flat), max(y_flat), 500)
+#plt.figure()
+#plt.contour(xtest, ytest, func_vcomp(xtest, ytest))
+#plt.show()
 
-### Using read_termini from MEaSUREs-validation.py
-#gl_termpos_fldr = 'Documents/GitHub/plastic-networks/Data/MEaSUREs-termini'
-#terminus_basefile = '/termini_0607_v01_2'
-#init_year = 2006
-#fn = gl_termpos_fldr + terminus_basefile #filename to read in for termini that will be traced
-#termini_init = read_termini(fn, init_year)
-#
-###iterate over keys in termini_init to make dictionary of lines for each GlacierID
-##ids_to_trace = termini_init.keys() #trace all points of all glaciers
+## Using read_termini from MEaSUREs-validation.py
+def read_termini(filename, year):
+    """Make a dictionary of terminus positions, indexed by MEaSUREs ID. Outputs dictionary"""
+    print 'Reading in MEaSUREs terminus positions for year ' + str(year)
+    sf = shapefile.Reader(filename)
+    fields = sf.fields[1:] #excluding the mute "DeletionFlag"
+    field_names = [field[0] for field in fields]
+    term_recs = sf.shapeRecords()
+    termpts_dict = {}
+    for r in term_recs:
+        atr = dict(zip(field_names, r.record)) #dictionary of shapefile fields, so we can access GlacierID by name rather than index.  Index changes in later years.
+        key = atr['GlacierID'] #MEaSUREs ID number for the glacier, found by name rather than index
+        termpts_dict[key] = np.asarray(r.shape.points) #save points spanning terminus to dictionary
+    return termpts_dict
+
+gl_termpos_fldr = 'Documents/GitHub/plastic-networks/Data/MEaSUREs-termini'
+terminus_basefile = '/termini_0607_v01_2'
+init_year = 2006
+fn = gl_termpos_fldr + terminus_basefile #filename to read in for termini that will be traced
+termini_init = read_termini(fn, init_year)
+
+##iterate over keys in termini_init to make dictionary of lines for each GlacierID
+#ids_to_trace = termini_init.keys() #trace all points of all glaciers
 #ids_to_trace = (3, 153, 175) # IDs for only Jakobshavn, Kangerlussuaq, Helheim
-#
-#all_lines = {}
-#for gid in ids_to_trace:
-#    lines = {}
-#    termcoords = termini_init[gid] #points spanning terminus for this glacier
-#    for j in range(len(termcoords)):
-#        p = termcoords[j]
-#        line_coords, width = Trace_wWidth(p[0], p[1], trace_up=True, xarr=x_comp, yarr=y_comp, Vx = func_vxcomp, Vy = func_vycomp, V = func_vcomp) #Uses Trace_wWidth and FilterMainTributaries from network_selection.py
-#        xyw = [(line_coords[n][0], line_coords[n][1], width[n]) for n in range(len(line_coords))]
-#        lines[j] = (xyw)
-#    filtered_tribs = FilterMainTributaries(lines, Vx = func_vxcomp, Vy = func_vycomp)
-#    all_lines[gid] = lines
+ids_to_trace = (3,) #Just Jakobshavn
+
+all_lines = {}
+for gid in ids_to_trace:
+    lines = {}
+    termcoords = termini_init[gid] #points spanning terminus for this glacier
+    for j in range(len(termcoords))[::5]: #need to down-sample, since some glaciers have 50 points across terminus (and we expect most will have fairly simple networks)
+        print 'Tracing terminus point {} of {} in Glacier ID {}'.format(j, len(termcoords), gid)
+        p = termcoords[j]
+        line_coords, width = Trace_wWidth(p[0], p[1], trace_up=True, xarr=x_comp, yarr=y_comp, Vx = func_vxcomp, Vy = func_vycomp, V = func_vcomp) #Uses Trace_wWidth and FilterMainTributaries from network_selection.py
+        xyw = [(line_coords[n][0], line_coords[n][1], width[n]) for n in range(len(line_coords))]
+        lines[j] = (xyw)
+    filtered_tribs = FilterMainTributaries(lines, Vx = func_vxcomp, Vy = func_vycomp)
+    all_lines[gid] = lines
 
         
