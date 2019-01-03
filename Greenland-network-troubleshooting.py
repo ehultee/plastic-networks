92ff6c1ce4587b039a9420109905d7cf5d84eb10
Lizz <ehultee@umich.edu>
2019-01-03 16:02:57 -0500

    Added analysis of unphysical terminus elevations
    Wrote functions to read in results of a yield-strength optimization on all Greenland outlet glaciers, extract data about troublesome glaciers, and plot centerline profiles of those glaciers.

--- Greenland-network-troubleshooting.py
+++ Greenland-network-troubleshooting.py
@@ -16,6 +16,9 @@ from matplotlib import cm
 #from shapely.geometry import *
 from scipy import interpolate
 from scipy.ndimage import gaussian_filter
+from plastic_utilities_v2 import *
+from GL_model_tools import *
+from flowline_class_hierarchy import *
 
 ##-------------------------------
 ##   READING IN & PROCESSING DATA
@@ -51,8 +54,6 @@ def read_velocities(filename, return_grid=True, return_proj=False):
         return x, y, varr
     else: 
         return varr
-
-
 
 ## Read in MEaSUREs velocity composite
 print 'Reading MEaSUREs velocities'
@@ -97,9 +98,12 @@ M = thick_mask[::2,::2]
 #S = ss
 fh.close()
 
+unsmoothB = B
+smoothB = gaussian_filter(B, 2)
+
 S_new = np.add(B, H) #alternative surface elevation: bed elevation + ice thickness
 
-S_interp = interpolate.RectBivariateSpline(X, Y[::-1], S[::, ::-1]) #interpolating surface elevation provided
+S_interp = interpolate.RectBivariateSpline(X, Y[::-1], S.T[::, ::-1]) #interpolating surface elevation provided
 #S_interp = interpolate.RectBivariateSpline(X, Y[::-1], S_new.T[::, ::-1]) #interpolating bed + thickness
 H_interp = interpolate.RectBivariateSpline(X, Y[::-1], H.T[::, ::-1])
 B_interp = interpolate.RectBivariateSpline(X, Y[::-1], smoothB.T[::, ::-1])
@@ -127,12 +131,103 @@ init_year = 2006
 fn = gl_termpos_fldr + terminus_basefile #filename to read in for termini that will be traced
 termini_init = read_termini(fn, init_year)
 
-IDs_stored_empty = (139, 140, 141, 142, 143, 159, 161, 172, 173, 177)
+
+## New function to read in CSV results of optimal-yield-strength analysis
+def read_optimization_analysis(filename, read_yieldtype=False):
+    """Read a CSV file listing optimal values of yield strength for auto-selected Greenland glaciers
+    Input: 
+        filename
+    Default arg: 
+        read_yieldtype=False: determines whether we want to read and save the yield type (constant vs. Coulomb variable)
+    Output: 
+        Dictionary of lists including
+        -Glacier ID (referenced to MEaSUREs Greenland outlets)
+        -Optimal tau_y
+        -Terminal bed
+        -Terminal surface elevation
+    """
+    
+    f = open(filename, 'r')
+    
+    header = f.readline() #header line
+    #hdr = header.strip('\r\n')
+    #keys = hdr.split(',') #get names of columns
+    #data = {k: [] for k in keys}
+    data = {'Glacier_IDs': [], #shorter keys than the names stored in CSV header
+    'Optimal_taus': [],
+    'Yieldtype': [],
+    'Terminal_bed': [],
+    'Terminal_SE': [],
+    'Terminal_H': []} #adding field for ice thickness
+    
+    lines = f.readlines()
+    f.close
+    
+    for i, l in enumerate(lines):
+        linstrip = l.strip('\r\n')
+        parts = linstrip.split(',')
+        
+        bed_el = float(parts[3])
+        surf_el = float(parts[4])
+        
+        data['Glacier_IDs'].append(int(parts[0]))
+        data['Optimal_taus'].append(float(parts[1]))
+        if read_yieldtype: #generally won't need this
+            data['Yieldtype'].append(parts[2])
+        else:
+            pass
+        data['Terminal_bed'].append(bed_el)
+        data['Terminal_SE'].append(surf_el)
+        data['Terminal_H'].append(surf_el-bed_el) #calculating ice thickness from reported bed and surface elevations
+    
+    return data
+
+analysis_fn = 'Documents/1. Research/2. Flowline networks/Auto_selected-networks/Optimization_analysis/bestfit_taus-fromdate_2018-11-02.csv'
+opt_data = read_optimization_analysis(analysis_fn)
+IDs_neg_termini = [g for i,g in enumerate(opt_data['Glacier_IDs']) if opt_data['Terminal_SE'][i]<0]
+IDs_thin_termini = [g for i,g in enumerate(opt_data['Glacier_IDs']) if opt_data['Terminal_H'][i]<10]   
+
+## Read in the networks of glaciers with problematic termini--based on Greenland-network_processing_routine.py
+base_fpath = 'Documents/1. Research/2. Flowline networks/Auto_selected-networks/Gld-autonetwork-GID'
+flowlines_foranalysis = []
+for gid in IDs_neg_termini:
+    print 'Reading in glacier ID: '+str(gid)
+    if gid<160:
+        filename = base_fpath+str(gid)+'-date_2018-10-03.csv'
+    else:
+        filename = base_fpath+str(gid)+'-date_2018-10-04.csv' #workaround because I ran these in batches and saved them with the date
+    
+    coords_list = Flowline_CSV(filename, has_width=True, flip_order=False)
+    nlines = len(coords_list)
+    fl = Flowline(coords=coords_list[0], name='GID'+str(gid), index=0) #saving central branch as main
+    print 'Now processing glacier ID: '+str(gid)
+    fl.process_bed(B_interp)
+    fl.process_surface(S_interp)
+    fl.process_thickness(H_interp)
+    flowlines_foranalysis.append(fl)
+    
+    
+    
+  
 ##-------------------------------
-##   PLOTTING
+##   EXPLORATORY PLOTTING
 ##-------------------------------
-plt.figure()
-plt.contourf(x_comp, y_comp[::], v_comp[::-1, ::])
-for gid in IDs_stored_empty:
-    plt.scatter(termini_init[gid][:,0], termini_init[gid][:,1], marker='*', color='r')
-plt.show()
+### Do networks stored empty have termini that lie 'off the map' of velocity for tracing?
+#IDs_stored_empty = (139, 140, 141, 142, 143, 159, 161, 172, 173, 177)
+#plt.figure()
+#plt.contourf(x_comp, y_comp[::], v_comp[::-1, ::])
+#for gid in IDs_stored_empty:
+#    plt.scatter(termini_init[gid][:,0], termini_init[gid][:,1], marker='*', color='r')
+#plt.show()
+### Yes, they do.
+
+
+## Do the surface profiles of glaciers with small/negative terminus elevation look wonky in other ways?
+#IDs_neg_termini = (3, 9, 13, 19, 20, 22, 29, 30, ) #Glacier IDs with negative terminus elevation according to bestfit_taus-fromdate_2018-11-02.csv (extracted surface elevation from interpolation of B+H)
+for fl in flowlines_foranalysis: 
+    xarr = linspace(0, fl.length, num=1000)   
+    plt.figure()
+    plt.plot(xarr, fl.bed_function(xarr), color='Chocolate')
+    plt.plot(xarr, fl.surface_function(xarr), color='Gainsboro')
+    plt.show()
+## Yes, many do look wonky.  Many also report a thickness (from fl.thickness_function) different from their surface-bed. More structured analysis to come.
