0af07614208a94b51231834dbdc31dc2e68a6d83
Lizz <ehultee@umich.edu>
2019-03-20 10:45:19 -0400

    Fixed broken file paths
    Changed file paths from GitHub/plastic-networks/Data to the new (private) GitHub/Data_unsynced.
    
    Commented out functions and data read-in that will not be relevant to validating Greenland hindcasts.

--- MEaSUREs-validation.py
+++ MEaSUREs-validation.py
@@ -1,5 +1,6 @@
 ## Loading in MEaSUREs terminus position data for Greenland to assess utility for validating hindcasts
 ## 12 Sept 2018  EHU
+## 20 Mar 2019 edit: use these functions to compare 2006-2014 termini with hindcasted
 
 from netCDF4 import Dataset
 import numpy as np
@@ -21,38 +22,38 @@ from flowline_class_hierarchy import *
 ### COMMENT OUT IF DATA IS ALREADY READ IN TO YOUR SESSION
 ##-------------------
 
-print 'Reading in surface topography'
-gl_bed_path ='Documents/1. Research/2. Flowline networks/Model/Data/BedMachine-Greenland/BedMachineGreenland-2017-09-20.nc'
-fh = Dataset(gl_bed_path, mode='r')
-xx = fh.variables['x'][:].copy() #x-coord (polar stereo (70, 45))
-yy = fh.variables['y'][:].copy() #y-coord
-s_raw = fh.variables['surface'][:].copy() #surface elevation
-h_raw=fh.variables['thickness'][:].copy() # Gridded thickness
-b_raw = fh.variables['bed'][:].copy() # bed topo
-thick_mask = fh.variables['mask'][:].copy()
-ss = np.ma.masked_where(thick_mask !=2, s_raw)#mask values: 0=ocean, 1=ice-free land, 2=grounded ice, 3=floating ice, 4=non-Greenland land
-hh = np.ma.masked_where(thick_mask !=2, h_raw) 
-bb = np.ma.masked_where(thick_mask !=2, b_raw)
-## Down-sampling
-X = xx[::2]
-Y = yy[::2]
-S = ss[::2, ::2]
-H = hh[::2, ::2] 
-B = bb[::2, ::2]
-## Not down-sampling
-#X = xx
-#Y = yy
-#S = ss
-fh.close()
-
-#Smoothing bed to check effect on dLdt
-unsmoothB = B
-smoothB = gaussian_filter(B, 2)
-#B_processed = np.ma.masked_where(thick_mask !=2, smoothB)
-
-S_interp = interpolate.RectBivariateSpline(X, Y[::-1], S.T[::, ::-1])
-H_interp = interpolate.RectBivariateSpline(X, Y[::-1], H.T[::, ::-1])
-B_interp = interpolate.RectBivariateSpline(X, Y[::-1], smoothB.T[::, ::-1])
+#print 'Reading in surface topography'
+#gl_bed_path ='Documents/1. Research/2. Flowline networks/Model/Data/BedMachine-Greenland/BedMachineGreenland-2017-09-20.nc'
+#fh = Dataset(gl_bed_path, mode='r')
+#xx = fh.variables['x'][:].copy() #x-coord (polar stereo (70, 45))
+#yy = fh.variables['y'][:].copy() #y-coord
+#s_raw = fh.variables['surface'][:].copy() #surface elevation
+#h_raw=fh.variables['thickness'][:].copy() # Gridded thickness
+#b_raw = fh.variables['bed'][:].copy() # bed topo
+#thick_mask = fh.variables['mask'][:].copy()
+#ss = np.ma.masked_where(thick_mask !=2, s_raw)#mask values: 0=ocean, 1=ice-free land, 2=grounded ice, 3=floating ice, 4=non-Greenland land
+#hh = np.ma.masked_where(thick_mask !=2, h_raw) 
+#bb = np.ma.masked_where(thick_mask !=2, b_raw)
+### Down-sampling
+#X = xx[::2]
+#Y = yy[::2]
+#S = ss[::2, ::2]
+#H = hh[::2, ::2] 
+#B = bb[::2, ::2]
+### Not down-sampling
+##X = xx
+##Y = yy
+##S = ss
+#fh.close()
+#
+##Smoothing bed to check effect on dLdt
+#unsmoothB = B
+#smoothB = gaussian_filter(B, 2)
+##B_processed = np.ma.masked_where(thick_mask !=2, smoothB)
+#
+#S_interp = interpolate.RectBivariateSpline(X, Y[::-1], S.T[::, ::-1])
+#H_interp = interpolate.RectBivariateSpline(X, Y[::-1], H.T[::, ::-1])
+#B_interp = interpolate.RectBivariateSpline(X, Y[::-1], smoothB.T[::, ::-1])
 
 ### Reading in SENTINEL velocity map
 #print 'Now reading in (vector) velocity map'
@@ -74,25 +75,25 @@ B_interp = interpolate.RectBivariateSpline(X, Y[::-1], smoothB.T[::, ::-1])
 #vf_x = interpolate.interp2d(xv, yv[::-1], vx_upper[::-1,::])
 #vf_y = interpolate.interp2d(xv, yv[::-1], vy_upper[::-1,::])
 #vf = interpolate.interp2d(xv, yv[::-1], v_upper[::-1, ::])
-
-print 'Reading in 5-year surface elevation change'
-gl_sec_path ='Documents/GitHub/plastic-networks/Data/CS2-SEC_5yr.nc'
-#gl_sec_path ='Documents/GitHub/plastic-networks/Data/CS2-SEC_2yr.nc'
-fh3 = Dataset(gl_sec_path, mode='r')
-x_sec = fh3.variables['x'][:].copy() #x-coord (polar stereo)
-y_sec = fh3.variables['y'][:].copy() #y-coord (polar stereo)
-t_sec = fh3.variables['t'][:].copy() #average time of slice (days since 1 JAN 2000)
-sec_raw = fh3.variables['SEC'][:].copy()
-fh3.close()
-
-sec_i_masked = np.ma.masked_greater(sec_raw[:,:,0], 9000)
-sec_i_excludemasked = np.ma.filled(sec_i_masked, fill_value=np.mean(sec_i_masked))
-#sec_i_regrid = interpolate.griddata((x_sec.ravel(), y_sec.ravel()), sec_i_masked.ravel(), (Xmat, Ymat), method='nearest')
-SEC_i = interpolate.RectBivariateSpline(x_sec, y_sec, sec_i_excludemasked.T)
+#
+#print 'Reading in 5-year surface elevation change'
+#gl_sec_path ='Documents/GitHub/Data_unsynced/CS2-SEC_5yr.nc'
+##gl_sec_path ='Documents/GitHub/Data_unsynced/CS2-SEC_2yr.nc'
+#fh3 = Dataset(gl_sec_path, mode='r')
+#x_sec = fh3.variables['x'][:].copy() #x-coord (polar stereo)
+#y_sec = fh3.variables['y'][:].copy() #y-coord (polar stereo)
+#t_sec = fh3.variables['t'][:].copy() #average time of slice (days since 1 JAN 2000)
+#sec_raw = fh3.variables['SEC'][:].copy()
+#fh3.close()
+#
+#sec_i_masked = np.ma.masked_greater(sec_raw[:,:,0], 9000)
+#sec_i_excludemasked = np.ma.filled(sec_i_masked, fill_value=np.mean(sec_i_masked))
+##sec_i_regrid = interpolate.griddata((x_sec.ravel(), y_sec.ravel()), sec_i_masked.ravel(), (Xmat, Ymat), method='nearest')
+#SEC_i = interpolate.RectBivariateSpline(x_sec, y_sec, sec_i_excludemasked.T)
 
 
 print 'Reading in MEaSUREs reference file' 
-gl_gid_fldr = 'Documents/GitHub/plastic-networks/Data/MEaSUREs-GlacierIDs'
+gl_gid_fldr = 'Documents/GitHub/Data_unsynced/MEaSUREs-GlacierIDs'
 sf_ref = shapefile.Reader(gl_gid_fldr+'/GlacierIDs_v01_2') #Specify the base filename of the group of files that makes up a shapefile
 
 
@@ -111,7 +112,7 @@ def read_termini(filename, year):
         termpts_dict[key] = np.asarray(r.shape.points) #save points spanning terminus to dictionary
     return termpts_dict
 
-gl_termpos_fldr = 'Documents/GitHub/plastic-networks/Data/MEaSUREs-termini'
+gl_termpos_fldr = 'Documents/GitHub/Data_unsynced/MEaSUREs-termini'
 basefiles = ['/termini_0001_v01_2', '/termini_0506_v01_2', '/termini_0607_v01_2', '/termini_0708_v01_2', '/termini_0809_v01_2', '/termini_1213_v01_2', '/termini_1415_v01_2', '/termini_1516_v01_2', '/termini_1617_v01_2']
 years = [2000, 2005, 2006, 2007, 2008, 2012, 2014, 2015, 2016]
 
@@ -122,50 +123,48 @@ for i,b in enumerate(basefiles):
     termini[yr] = read_termini(fn, yr) #creating dictionary for each year
     print len(termini[yr])
     
-## Test earliest year of appearance for each glacier
-master_initial_termini = {}
-keys_05 = []
-keys_06 = []
-keys_07 = []
-keys_08 = []
-keys_12 = []
-
-for k in termini[2014].keys():
-    if k in termini[2000].keys():
-        master_initial_termini[k] = termini[2000][k]
-    elif k in termini[2005].keys():
-        print 'Glacier ID ' + str(k) + ' taken from year 2005'
-        master_initial_termini[k] = termini[2005][k]
-        keys_05.append(k)
-    elif k in termini[2006].keys():
-        print 'Glacier ID ' + str(k) + ' taken from year 2006'
-        master_initial_termini[k] = termini[2006][k]
-        keys_06.append(k)
-    elif k in termini[2007].keys():
-        print 'Glacier ID ' + str(k) + ' taken from year 2007'
-        master_initial_termini[k] = termini[2007][k]
-        keys_07.append(k)
-    elif k in termini[2008].keys():
-        print 'Glacier ID ' + str(k) + 'taken from year 2008'
-        master_initial_termini[k] = termini[2008][k]
-        keys_08.append(k)
-    elif k in termini[2012].keys():
-        print 'Glacier ID ' + str(k) + ' taken from year 2012'
-        master_initial_termini[k] = termini[2012][k]
-        keys_12.append(k)
-    else:
-        print 'Glacier ID ' + str(k) + ' not found before 2014'
-    
-
-#print 'Reading in MEaSUREs terminus positions for year 2000'
-#gl_termpos_fldr = 'Documents/GitHub/plastic-networks/Data/MEaSUREs-termini'
-#sf_termpos = shapefile.Reader(gl_termpos_fldr+'/termini_0001_v01_2') #Specify the base filename of the group of files that makes up a shapefile
-
-print 'Reading in MEaSUREs terminus positions for year 2014'
-gl_termpos_fldr = 'Documents/GitHub/plastic-networks/Data/MEaSUREs-termini'
-sf_termpos_1415 = shapefile.Reader(gl_termpos_fldr+'/termini_1415_v01_2') #Specify the base filename of the group of files that makes up a shapefile
-
-print 'Reading in MEaSUREs terminus positions for year 2015'
-gl_termpos_fldr = 'Documents/GitHub/plastic-networks/Data/MEaSUREs-termini'
-sf_termpos_1516 = shapefile.Reader(gl_termpos_fldr+'/termini_1516_v01_2') #Specify the base filename of the group of files that makes up a shapefile
+### Test earliest year of appearance for each glacier
+#master_initial_termini = {}
+#keys_05 = []
+#keys_06 = []
+#keys_07 = []
+#keys_08 = []
+#keys_12 = []
+#
+#for k in termini[2014].keys():
+#    if k in termini[2000].keys():
+#        master_initial_termini[k] = termini[2000][k]
+#    elif k in termini[2005].keys():
+#        print 'Glacier ID ' + str(k) + ' taken from year 2005'
+#        master_initial_termini[k] = termini[2005][k]
+#        keys_05.append(k)
+#    elif k in termini[2006].keys():
+#        print 'Glacier ID ' + str(k) + ' taken from year 2006'
+#        master_initial_termini[k] = termini[2006][k]
+#        keys_06.append(k)
+#    elif k in termini[2007].keys():
+#        print 'Glacier ID ' + str(k) + ' taken from year 2007'
+#        master_initial_termini[k] = termini[2007][k]
+#        keys_07.append(k)
+#    elif k in termini[2008].keys():
+#        print 'Glacier ID ' + str(k) + 'taken from year 2008'
+#        master_initial_termini[k] = termini[2008][k]
+#        keys_08.append(k)
+#    elif k in termini[2012].keys():
+#        print 'Glacier ID ' + str(k) + ' taken from year 2012'
+#        master_initial_termini[k] = termini[2012][k]
+#        keys_12.append(k)
+#    else:
+#        print 'Glacier ID ' + str(k) + ' not found before 2014'
+ 
+       
+#gl_termpos_fldr = 'Documents/GitHub/Data_unsynced/MEaSUREs-termini'
+##print 'Reading in MEaSUREs terminus positions for year 2000'
+##sf_termpos = shapefile.Reader(gl_termpos_fldr+'/termini_0001_v01_2') #Specify the base filename of the group of files that makes up a shapefile
+#
+#print 'Reading in MEaSUREs terminus positions for year 2014'
+#sf_termpos_1415 = shapefile.Reader(gl_termpos_fldr+'/termini_1415_v01_2') #Specify the base filename of the group of files that makes up a shapefile
+#
+#print 'Reading in MEaSUREs terminus positions for year 2015'
+#sf_termpos_1516 = shapefile.Reader(gl_termpos_fldr+'/termini_1516_v01_2') #Specify the base filename of the group of files that makes up a shapefile
 
