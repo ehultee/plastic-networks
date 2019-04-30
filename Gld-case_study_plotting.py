772e1d70acdf2fff93172e98709f4f9dc2c54742
Lizz <ehultee@umich.edu>
2019-04-30 17:22:56 -0400

    Fixed plotting of max/min terminus
    Refined settings on plt.errorbar to correctly display max/min observed termini.

--- Gld-case_study_plotting.py
+++ Gld-case_study_plotting.py
@@ -150,16 +150,16 @@ def retterm(termset, linestr):
     for i in xrange(len(x_term)):
         proji = linestr.project(Point(x_term[i], y_term[i]))
         projections.append(proji)
-    termmin = max(projections) #need minimum rather than max here because we are interested in the most advanced, i.e. lowest arc-length value projection of terminus
+    termmin = max(projections) #max is the most retreated, i.e. highest arc-length value projection of terminus
     return termmin/1000
 
 gl_termpos_fldr = 'Documents/GitHub/Data_unsynced/MEaSUREs-termini'
 basefiles = ['/termini_0607_v01_2', '/termini_0708_v01_2', '/termini_0809_v01_2', '/termini_1213_v01_2', '/termini_1415_v01_2', '/termini_1516_v01_2']
-years = [2006, 2007, 2008, 2012, 2014, 2015] #compare with term of hindcast, 2006-2014
+obs_years = [2006, 2007, 2008, 2012, 2014, 2015] #compare with term of hindcast, 2006-2014
 
 termini = {}
 for i,b in enumerate(basefiles):
-    yr = years[i]
+    yr = obs_years[i]
     fn = gl_termpos_fldr+b
     termini[yr] = read_termini(fn, yr) #creating dictionary for each year
     print len(termini[yr])
@@ -178,125 +178,125 @@ for gid in glaciers_to_plot:
     
     coords_list = Flowline_CSV(filename, has_width=True, flip_order=False)
     mainline = LineString(coords_list[0])
-    for yr in years:
+    for yr in obs_years:
         termpts = termini[yr][gid] #get terminus points for each year
         t = projected_term_obs(termpts, mainline) #project onto main flowline
-        projected_termini[gid].append(t) #add this to dictionary of projected termini per glacier
+        r = retterm(termpts, mainline) #find most retreated point
+        a = advterm(termpts, mainline) #find most advanced point
+        projected_termini[gid].append((a, t, r)) #add these to dictionary of projected termini per glacier
 
     
     
+
+###--------------------------------------
+#### PLOTTING
+###--------------------------------------   
+  
+## Settings for plots   
+labels = [str(g[1]) for g in gids_by_name if g[0] in glaciers_to_plot] #set what the glaciers will be called in plotting.  Default is simply their MEaSUREs ID
+markers = ['o', '.', ',', '^', 'd', '*']
+styles = ['-', ':', '-.', '-', '-', '-']
+cmap = cm.get_cmap('winter')
+scenario_colors = cm.get_cmap('Blues')([0.1, 0.3, 0.5, 0.7, 0.9])
+#colors = cmap([0.1, 0.2, 0.3, 0.5, 0.7, 0.9])
+colors = cmap(linspace(0.1, 0.9, num=len(glaciers_to_plot)))
+alt_colors = cm.get_cmap('Greys')([0.2, 0.3, 0.5, 0.7, 0.9])
+plot_years = 2006+np.array(testyears)
+
+
 #
-####--------------------------------------
-##### PLOTTING
-####--------------------------------------   
-#  
-### Settings for plots   
-#labels = [str(g[1]) for g in gids_by_name if g[0] in glaciers_to_plot] #set what the glaciers will be called in plotting.  Default is simply their MEaSUREs ID
-#markers = ['o', '.', ',', '^', 'd', '*']
-#styles = ['-', ':', '-.', '-', '-', '-']
-#cmap = cm.get_cmap('winter')
-#scenario_colors = cm.get_cmap('Blues')([0.1, 0.3, 0.5, 0.7, 0.9])
-##colors = cmap([0.1, 0.2, 0.3, 0.5, 0.7, 0.9])
-#colors = cmap(linspace(0.1, 0.9, num=len(glaciers_to_plot)))
-#alt_colors = cm.get_cmap('Greys')([0.2, 0.3, 0.5, 0.7, 0.9])
-#
-##plt.figure()
-##for j in range(len(perscenario_SLE)):
-##    plt.plot(testyears, perscenario_SLE[j], color=alt_colors[j+2], label=scenarios[j])
-###for k in range(len(coarser_SLE)):
-###    plt.plot(arange(100, step=0.5), coarser_SLE[k], color=scenario_colors[k+3], label=scenarios[k+3])
-##plt.legend(loc='upper left')
-##plt.axes().set_xlabel('Year of simulation', size=20)
-##plt.axes().set_ylabel('Cumulative sea level contribution [mm]', size=20)
-##plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
-###plt.axes().set_xlim(0, 100)
-###plt.axes().set_xticks([0, 25, 50, 75, 100])
-####plt.axes().set_ylim(-16, 1)
-####plt.axes().set_yticks([0, 1, 2, 3, 4])
-##plt.show()
-#
-#
-#####-------------------
-###### INDIVIDUAL SCENARIO BREAKDOWNS
-#####-------------------
-#
-###projections_WB = [Jak_main_warmbaseline.model_output, Jak_sec_warmbaseline.model_output, Jak_tert_warmbaseline.model_output, KogeBugt_warmbaseline.model_output, Helheim_warmbaseline.model_output, Kanger_warmbaseline.model_output]
-###projections_CB = [Jak_main_coldbaseline.model_output, Jak_sec_coldbaseline.model_output, Jak_tert_coldbaseline.model_output, KogeBugt_coldbaseline.model_output, Helheim_coldbaseline.model_output, Kanger_coldbaseline.model_output]
+####terminus
+plt.figure()
+for j, gid in enumerate(glaciers_to_plot):
+    print gid
+    term_positions = full_output_dicts['persistence']['GID{}'.format(gid)][0]['Termini'][1::]
+    ms_selection = mod(j, len(styles))
+    plt.plot(testyears, -0.001*np.array(term_positions), linewidth=2, color='Gainsboro', linestyle=styles[ms_selection], label=labels[j])
+    #plt.plot(testyears[::4], -0.001*np.array(full_output_dicts['persistence']['GID{}'.format(gid)][0]['Termini'][1::])[::4], linewidth=0, marker=markers[ms_selection], ms=10, color=colors[j])
+plt.legend(loc='lower left')
+plt.axes().set_xlabel('Year of simulation', size=20)
+plt.axes().set_ylabel('Terminus change [km]', size=20)
+plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
+#plt.axes().set_ylim(-100, 1)
+#plt.axes().set_yticks([-75, -50, -25, 0])
+plt.title('Terminus retreat of {} Greenland outlet glaciers 2006-2014 ERA-I, Tice=-10 C'.format(len(glaciers_to_plot)), fontsize=20)
+plt.show()
+
+####SINGLE NETWORK - termini vs obs
+sim_termini = full_output_dicts['persistence']['GID3'][0]['Termini'][1::]
+obs_termini = np.asarray(projected_termini[3]) #will be of shape (len(obs_years), 3) with an entry (lower, centroid, upper) for each year
+obs_term_centr = obs_termini[:,1]
+e = np.asarray([(min(ot[0]-ot[1], ot[0]), ot[1]-ot[2]) for ot in obs_termini]).T #error lower (more advanced), upper (less advanced)
+plt.figure('Single network terminus change')
+plt.plot(plot_years, -0.001*np.array(sim_termini), linewidth=2, color='k', linestyle='--', label='Modelled')
+plt.errorbar(obs_years, -1*obs_term_centr, yerr = e, fmt='D')
+plt.axes().set_xlabel('Year', size=30)
+plt.axes().set_ylabel('Terminus change [km]', size=30)
+plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
+plt.axes().set_xlim(2006, 2014.5)
+plt.axes().set_xticks([2006, 2008, 2010, 2012, 2014])
+plt.axes().set_xticklabels(['2006', '2008', '2010', '2012', '2014'])
+#plt.axes().set_ylim(-50, 1)
+#plt.axes().set_yticks([-50, -25, 0])
+plt.show()
+
+
 ##
-##
-#####terminus
-#plt.figure()
-#for j, gid in enumerate(glaciers_to_plot):
-#    print gid
-#    term_positions = full_output_dicts['persistence']['GID{}'.format(gid)][0]['Termini'][1::]
-#    ms_selection = mod(j, len(styles))
-#    plt.plot(testyears, -0.001*np.array(term_positions), linewidth=2, color='Gainsboro', linestyle=styles[ms_selection], label=labels[j])
-#    #plt.plot(testyears[::4], -0.001*np.array(full_output_dicts['persistence']['GID{}'.format(gid)][0]['Termini'][1::])[::4], linewidth=0, marker=markers[ms_selection], ms=10, color=colors[j])
+#####SINGLE NETWORK - splitting termini
+#single_network_output = full_output_dicts['persistence']['GID10']
+#plt.figure('Single network terminus change')
+#for k in range(len(single_network_output)): #for each branch j
+#    #colork = alt_colors[mod(k, len(colors))]
+#    branch_termini = single_network_output[k]['Termini'] #for some reason enumerate doesn't work with loaded-in output, so we're stuck with this
+#    #markerk = (k+2, mod(k+1, 3), 0)
+#    plt.plot(testyears, -0.001*np.array(branch_termini[0:-1:]), linewidth=4, label='Branch {}'.format(k))
+#    #plt.plot(testyears[::10], -0.001*np.array(branch_termini[0:-1:])[::10], linewidth=0, color=colork, marker=markerk, ms=10)
 #plt.legend(loc='lower left')
-#plt.axes().set_xlabel('Year of simulation', size=20)
-#plt.axes().set_ylabel('Terminus change [km]', size=20)
+#plt.axes().set_xlabel('Year of simulation', size=30)
+#plt.axes().set_ylabel('Terminus change [km]', size=30)
 #plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
-##plt.axes().set_ylim(-100, 1)
-##plt.axes().set_yticks([-75, -50, -25, 0])
-#plt.title('Terminus retreat of {} Greenland outlet glaciers 2006-2014 ERA-I, Tice=-10 C'.format(len(glaciers_to_plot)), fontsize=20)
+##plt.axes().set_ylim(-50, 1)
+##plt.axes().set_yticks([-50, -25, 0])
 #plt.show()
-##
-###
-######SINGLE NETWORK - splitting termini
-##single_network_output = full_output_dicts['persistence']['GID10']
-##plt.figure('Single network terminus change')
-##for k in range(len(single_network_output)): #for each branch j
-##    #colork = alt_colors[mod(k, len(colors))]
-##    branch_termini = single_network_output[k]['Termini'] #for some reason enumerate doesn't work with loaded-in output, so we're stuck with this
-##    #markerk = (k+2, mod(k+1, 3), 0)
-##    plt.plot(testyears, -0.001*np.array(branch_termini[0:-1:]), linewidth=4, label='Branch {}'.format(k))
-##    #plt.plot(testyears[::10], -0.001*np.array(branch_termini[0:-1:])[::10], linewidth=0, color=colork, marker=markerk, ms=10)
-##plt.legend(loc='lower left')
-##plt.axes().set_xlabel('Year of simulation', size=30)
-##plt.axes().set_ylabel('Terminus change [km]', size=30)
-##plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
-###plt.axes().set_ylim(-50, 1)
-###plt.axes().set_yticks([-50, -25, 0])
-##plt.show()
-##
-######Flux
-##for j, gid in enumerate(glaciers_to_plot):
-##    plt.figure('GID{}'.format(gid))
-##    ms_selection = mod(gid, len(styles))
-##    plt.plot(testyears, 1E-12*np.array(pernetwork_cumul_fx[j]), linewidth=4, label=str(gid), color=colors[ms_selection], linestyle = styles[ms_selection])
-##    plt.plot(testyears[::20], 1E-12*np.array(pernetwork_cumul_fx[j][::20]), linewidth=0, ms=10, marker=markers[ms_selection])
-##    plt.fill_between(testyears, y1=1E-12*np.array(pernetwork_cumul_fx[j]), y2=0, alpha=0.5)
-##    plt.axes().set_xlabel('Year of simulation', size=20)
-##    plt.axes().set_ylabel('Cumulative ice flux [Gt]', size=20)
-##    plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
-##    plt.axes().set_xlim(0,50)
-##    plt.axes().set_xticks([0,10,20, 30, 40, 50])
-##    #plt.axes().set_ylim(0, 401)
-##    #plt.axes().set_yticks([0, 50, 100, 150, 200, 250, 300, 350, 400])
-##    plt.show()
-#    ##
-########Sea level equivalent
-##plt.figure(figsize=(12,8))
-##for j, gid in enumerate(glaciers_to_plot):
-##    #if gid!=10:
-##    ms_selection = mod(gid, len(styles))
-##    plt.plot(testyears[::], scenario_sle[j], linewidth=4, color=colors[ms_selection], label=gid)
-##    plt.plot(testyears[::5], scenario_sle[j][::5], linewidth=0, marker=markers[ms_selection], ms=10, color=colors[ms_selection])
-##    if j==0:
-##        plt.fill_between(testyears[::], y1=scenario_sle[j], y2=0, color=colors[ms_selection], alpha=0.7)  
-##    else:
-##        plt.fill_between(testyears[::], y1=scenario_sle[j], y2=scenario_sle[j-1], color=colors[ms_selection], alpha=0.7)     
-###plt.plot([0, 20, 40, 60], [0, 14, 28, 42], color='k', linewidth=1, ls='-', alpha=0.8) #GRACE linear trend
-###rect = mpatches.Rectangle((98,8.5), width=2, height=4.6, color='k', alpha=0.7) # Nick et al total projection for 2100, A1B
-###rect2 = mpatches.Rectangle((98,11.3), width=2, height=6.2, color='k', alpha=0.7) # Nick et al total projection for 2100, RCP8.5 
-###plt.axes().add_patch(rect)
-###plt.axes().add_patch(rect2)
-##plt.legend(loc='upper left')
-##plt.axes().set_xlabel('Year of simulation', size=20)
-##plt.axes().set_ylabel('Cumulative sea level contribution [mm]', size=20)
-##plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
-###plt.axes().set_xlim(0, 100)
-###plt.axes().set_xticks([0, 25, 50, 75, 100])
-###plt.axes().set_ylim(0, 12)
-###plt.axes().set_yticks([0, 2, 4, 6, 8, 10, 12])
-##plt.show()
+#
+#####Flux
+#for j, gid in enumerate(glaciers_to_plot):
+#    plt.figure('GID{}'.format(gid))
+#    ms_selection = mod(gid, len(styles))
+#    plt.plot(testyears, 1E-12*np.array(pernetwork_cumul_fx[j]), linewidth=4, label=str(gid), color=colors[ms_selection], linestyle = styles[ms_selection])
+#    plt.plot(testyears[::20], 1E-12*np.array(pernetwork_cumul_fx[j][::20]), linewidth=0, ms=10, marker=markers[ms_selection])
+#    plt.fill_between(testyears, y1=1E-12*np.array(pernetwork_cumul_fx[j]), y2=0, alpha=0.5)
+#    plt.axes().set_xlabel('Year of simulation', size=20)
+#    plt.axes().set_ylabel('Cumulative ice flux [Gt]', size=20)
+#    plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
+#    plt.axes().set_xlim(0,50)
+#    plt.axes().set_xticks([0,10,20, 30, 40, 50])
+#    #plt.axes().set_ylim(0, 401)
+#    #plt.axes().set_yticks([0, 50, 100, 150, 200, 250, 300, 350, 400])
+#    plt.show()
+    ##
+#######Sea level equivalent
+#plt.figure(figsize=(12,8))
+#for j, gid in enumerate(glaciers_to_plot):
+#    #if gid!=10:
+#    ms_selection = mod(gid, len(styles))
+#    plt.plot(testyears[::], scenario_sle[j], linewidth=4, color=colors[ms_selection], label=gid)
+#    plt.plot(testyears[::5], scenario_sle[j][::5], linewidth=0, marker=markers[ms_selection], ms=10, color=colors[ms_selection])
+#    if j==0:
+#        plt.fill_between(testyears[::], y1=scenario_sle[j], y2=0, color=colors[ms_selection], alpha=0.7)  
+#    else:
+#        plt.fill_between(testyears[::], y1=scenario_sle[j], y2=scenario_sle[j-1], color=colors[ms_selection], alpha=0.7)     
+##plt.plot([0, 20, 40, 60], [0, 14, 28, 42], color='k', linewidth=1, ls='-', alpha=0.8) #GRACE linear trend
+##rect = mpatches.Rectangle((98,8.5), width=2, height=4.6, color='k', alpha=0.7) # Nick et al total projection for 2100, A1B
+##rect2 = mpatches.Rectangle((98,11.3), width=2, height=6.2, color='k', alpha=0.7) # Nick et al total projection for 2100, RCP8.5 
+##plt.axes().add_patch(rect)
+##plt.axes().add_patch(rect2)
+#plt.legend(loc='upper left')
+#plt.axes().set_xlabel('Year of simulation', size=20)
+#plt.axes().set_ylabel('Cumulative sea level contribution [mm]', size=20)
+#plt.axes().tick_params(axis='both', length=5, width=2, labelsize=20)
+##plt.axes().set_xlim(0, 100)
+##plt.axes().set_xticks([0, 25, 50, 75, 100])
+##plt.axes().set_ylim(0, 12)
+##plt.axes().set_yticks([0, 2, 4, 6, 8, 10, 12])
+#plt.show()
