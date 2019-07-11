f00e5208663314ef1bf86a70f74448bea35554b9
Lizz <ehultee@umich.edu>
2019-07-11 13:42:35 -0400

    Added MidpointNormalize function
    Function written by Joe Kington (Matplotlib docs) to impose a user-defined midpoint on e.g. a diverging colorbar

--- GL_model_tools.py
+++ GL_model_tools.py
@@ -10,6 +10,7 @@ from scipy import spatial
 #from scipy.ndimage import gaussian_filter
 from mpl_toolkits.basemap import Basemap
 import mpl_toolkits.basemap.pyproj as pyproj
+import matplotlib.colors as colors
 import shapefile
 from osgeo import gdal
 import cPickle as pickle
@@ -422,6 +423,23 @@ def flowline_latlon(coords, fromproj=pyproj.Proj("+init=epsg:3413"), toproj=pypr
     x_lon, y_lat = pyproj.transform(fromproj, toproj, xs, ys)
     latlon_coords = np.asarray(zip(x_lon, y_lat))
     return latlon_coords
+
+# set the colormap and centre the colorbar - from Joe Kington (StOv)
+class MidpointNormalize(colors.Normalize):
+	"""
+	Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+
+	e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+	"""
+	def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+		self.midpoint = midpoint
+		colors.Normalize.__init__(self, vmin, vmax, clip)
+
+	def __call__(self, value, clip=None):
+		# I'm ignoring masked values and all kinds of edge cases to make a
+		# simple example...
+		x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+		return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
 
 ##--------------------------
 ## GREENLAND-SPECIFIC FILE I/O
