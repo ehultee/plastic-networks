f2c4e8146a9f5b0356287d7be2f0de052c259b45
Lizz Ultee [air] <ehultee@umich.edu>
2020-09-24 11:49:00 -0400

    Plot flotation runs
    Read in terminus positions and compute rate of advance/retreat for simulations imposing the flotation condition.  Plot with yield condition data.

--- Visualization/lsq_dLdt_visual.py
+++ Visualization/lsq_dLdt_visual.py
@@ -16,6 +16,7 @@ import pylab as plt
 # Input file for observations
 obs_data = np.loadtxt('/Users/lizz/Desktop/Hindcasted_networks/observed_terminus_centroids.csv',delimiter=',',skiprows=1)
 sim_data = np.loadtxt('/Users/lizz/Desktop/Hindcasted_networks/dense_simulated_termini.csv',delimiter=',',skiprows=1)
+lb_data = np.loadtxt('/Users/lizz/Desktop/Hindcasted_networks/lowerbound_termini.csv',delimiter=',',skiprows=1)
 
 # # Floating bits
 # float_data = np.loadtxt('/Users/lizz/Desktop/flotation_lengths.csv',delimiter=',',skiprows=1)
@@ -23,9 +24,9 @@ sim_data = np.loadtxt('/Users/lizz/Desktop/Hindcasted_networks/dense_simulated_t
 # tongue_removed = id[float_length>1000.0] # glacier ID where a floating tongue was removed
 
 # # For looping through the glacier ids
-# min_glacier_id = 1
-# max_glacier_id = 194
-glaciers_to_plot=[2, 9, 36, 42, 66, 68, 69, 70, 72, 75, 76, 78, 98, 103, 105, 112, 116, 127, 137, 154, 164, 175]
+min_glacier_id = 1
+max_glacier_id = 194
+# glaciers_to_plot=[2, 9, 36, 42, 66, 68, 69, 70, 72, 75, 76, 78, 98, 103, 105, 112, 116, 127, 137, 154, 164, 175]
 
 
 # Initialize arrays for dLdt and error in dLdt
@@ -33,21 +34,25 @@ dLdt_obs = []
 dLdt_obs_err = []
 dLdt_sim = []
 dLdt_sim_err = []
+dLdt_lb = []
+dLdt_lb_err = []
 
 glacier_id_advancing = []
 glacier_id_all = []
 fiddled = []
 count = 0
-# for glacier_id in range(min_glacier_id,max_glacier_id+1):
-for glacier_id in glaciers_to_plot:
+for glacier_id in range(min_glacier_id,max_glacier_id+1):
+# for glacier_id in glaciers_to_plot:
     # Pull out data for each glacier id
     f1=obs_data[:,0]==glacier_id
     f2=sim_data[:,0]==glacier_id
+    f3=lb_data[:,0]==glacier_id
     # Only compute if we have more than 2 measurements??
     if sum(f1)>2:
         # Least squares fit to observations
         m1,b,rvalue,pvalue1,err1=linregress(obs_data[f1,1],obs_data[f1,2])
         m2,b,rvalue,pvalue2,err2=linregress(sim_data[f2,1],sim_data[f2,2])
+        m3,b,rvalue,pvalue3,err3=linregress(lb_data[f3,1],lb_data[f3,2])
 
         # This will filter out weird simulation data
         if pvalue2>0.05:
@@ -57,6 +62,7 @@ for glacier_id in glaciers_to_plot:
             if ~np.isnan(m1):
                 dLdt_obs.append(m1);dLdt_obs_err.append(err1)
                 dLdt_sim.append(m2);dLdt_sim_err.append(err2)
+                dLdt_lb.append(m3);dLdt_lb_err.append(err2)
                 # Stack glacier ids  of glaciers that pass the test
                 glacier_id_all.append(glacier_id)
 
@@ -74,6 +80,8 @@ dLdt_obs = np.array(dLdt_obs)
 dLdt_sim = np.array(dLdt_sim)
 dLdt_obs_err = np.array(dLdt_obs_err)
 dLdt_sim_err = np.array(dLdt_sim_err)
+dLdt_lb = np.array(dLdt_lb)
+dLdt_lb_err = np.array(dLdt_lb_err)
 # fiddled=True means glaciers that have had a tongue removed
 fiddled=np.array(fiddled)
 
@@ -86,8 +94,9 @@ plt.fill_between(dLdt_range,dLdt_range,-2.1,color='Gainsboro',alpha=0.4)
 plt.errorbar(dLdt_obs,dLdt_sim,xerr=dLdt_obs_err,yerr=dLdt_sim_err,color='k',fmt='.',ecolor='lightgray',capsize=5)
 # plt.errorbar(dLdt_obs[fiddled],dLdt_sim[fiddled],xerr=dLdt_obs_err[fiddled],yerr=dLdt_sim_err[fiddled],color='k',fmt='.',ecolor='lightgray',capsize=5)
 # plt.errorbar(dLdt_obs[~fiddled],dLdt_sim[~fiddled],xerr=dLdt_obs_err[~fiddled],yerr=dLdt_sim_err[~fiddled],color='r',fmt='.',markersize=3,ecolor='lightgray',capsize=5)
-for gid, x, y in zip(sorted(list(set(obs_data[:,0]))), dLdt_obs, dLdt_sim):
-    plt.text(x, y, str(gid), color='r', fontsize=12)
+plt.errorbar(dLdt_obs,dLdt_lb,xerr=dLdt_obs_err,yerr=dLdt_lb_err,color='k',fmt='D',ecolor='lightgray',capsize=5)
+# for gid, x, y in zip(sorted(list(set(obs_data[:,0]))), dLdt_obs, dLdt_sim):
+#     plt.text(x, y, str(gid), color='r', fontsize=12)
 plt.axhline(0, color='black',linestyle='--')
 plt.axvline(0, color='black',linestyle='--')
 plt.xlabel('Observed rate of advance (km/a)')
